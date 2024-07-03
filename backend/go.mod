9e7d55b068cbf9059ebfe24e5611c5ea3df8cc24
Krupnov <pavelkm10@gmail.com>
2024-07-03 17:01:06 +0300

    fix docker connect

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,5 @@
 module github.com/krup4/face-feature-recognition/backend
 
-go 1.21.3
+go 1.22.4
 
 require github.com/gorilla/mux v1.8.1
--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	logger := slog.Default()
 
-	s := NewServer("localhost:8080", logger)
+	s := NewServer("0.0.0.0:8080", logger)
 
 	err := s.Start()
 
